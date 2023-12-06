b109236ba6f647ace674dfa7c998fbe86b7db5c1
KevinHaeusler <kevin.haeusler@khdev.ch>
2023-12-06 16:58:50 +0100

    tailwindcss hack

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,46 @@
-// JavaScript files are compiled and minified during the build process to the assets/built folder. See available scripts in the package.json file.
+// Function to set the theme based on localStorage
+function setTheme(theme) {
+  if (theme === 'dark') {
+    document.documentElement.classList.add('dark')
+    document.getElementById('theme-icon').classList.add('hidden')
+    document.getElementById('dark-theme-icon').classList.remove('hidden')
+    document.getElementById('theme-text').textContent = 'Dark'
+  } else {
+    document.documentElement.classList.remove('dark')
+    document.getElementById('theme-icon').classList.remove('hidden')
+    document.getElementById('dark-theme-icon').classList.add('hidden')
+    document.getElementById('theme-text').textContent = 'Light'
+  }
+}
 
-// Import CSS
-import "../css/index.css";
+// Function to toggle between dark and light themes
+function toggleTheme() {
+  const currentTheme = document.documentElement.classList.contains('dark') ? 'dark' : 'light'
+  const newTheme = currentTheme === 'dark' ? 'light' : 'dark'
 
-// Import JS
-import menuOpen from "./menuOpen";
-import infiniteScroll from "./infiniteScroll";
+  setTheme(newTheme)
+  localStorage.theme = newTheme
+}
 
+// Function to initialize the theme based on localStorage
+function initializeTheme() {
+  const savedTheme = localStorage.theme
+  if (
+    savedTheme === 'dark' ||
+    (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
+  ) {
+    setTheme('dark')
+  } else {
+    setTheme('light')
+  }
+}
 
-// Call the menu and infinite scroll functions
-menuOpen();
-infiniteScroll();
+// Event listener for theme toggle click
+const themeToggle = document.getElementById('theme-toggle')
+themeToggle.addEventListener('click', (event) => {
+  event.preventDefault()
+  toggleTheme()
+})
+
+// Initialize the theme on page load
+initializeTheme()
